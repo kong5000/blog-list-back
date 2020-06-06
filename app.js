6883d4a62bcd90b931cce78099c8c1517c8d1708
Keith <keith.ong5000@gmail.com>
2020-06-05 22:22:45 -0700

    Remove static front end end point, back and front end to be hosted seperately

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ console.log(config.MONGODB_URI)
 
 app.use(bodyParser.json())
 app.use(cors())
-// Whenever express gets a GET request, first check the build directory to
-// serve the front end
-app.use(express.static('build'))
+
 //Instead of doing app.get('/api/blog... use a router to seperate code.
 app.use(blogsRouter)
 app.use(usersRouter)
